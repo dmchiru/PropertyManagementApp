b279f7d79de27a0de3ae8aa436c706a61684e182
Dianelsa Chiru Morales <dmchiru@eagles.usi.edu>
2026-04-28 02:14:47 -0500

    Update

--- PropertyManagementApp.Client/Program.cs
+++ PropertyManagementApp.Client/Program.cs
@@ -1,20 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PropertyManagementApp.Client;
-
 using PropertyManagementApp.Client.Services;
 
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton<NotificationService>();
 
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"];
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:7108/")
+    BaseAddress = new Uri(apiBaseUrl!)
 });
 
 await builder.Build().RunAsync();
